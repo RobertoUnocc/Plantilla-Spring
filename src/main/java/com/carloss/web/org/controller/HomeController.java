134c8040b33573f59cef6edc515265056c235148
Roberto Unocc <carlitos2017unocc@gmail.com>
2022-12-28 14:25:21 -0500

    Plantilla V1.0

---
+++ src/main/java/com/carloss/web/org/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.carloss.web.org.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		return "home";
+	}
+}
